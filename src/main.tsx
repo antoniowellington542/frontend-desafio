d35b2a7d4cab9b7b0d27f95c885b869423d83189
Antonio Wellington <antoniowellington542@gmail.com>
2022-09-02 21:20:31 -0300

    refactor: requests

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+
+const root = createRoot(rootElement as Element);
+
+root.render(
     <App />
-  </React.StrictMode>
 )
